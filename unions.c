dfc4b687501819d654b21dcd1f449d42f555c4e9
Sean Moylan <sean.cheezy.moylan@gmail.com>
2020-02-18 10:49:20 +0000

    Added endian_test.c that checks for big or little endian

--- unions.c
+++ unions.c
@@ -4,17 +4,24 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-
 union endian {
   uint8_t ei[2];
   uint16_t si;
 };
 
 int main(int argc, char *argv) {
-  
+
+  // Create an instance of a union
   union endian myunion;
 
+  // Assign a variable by using myunion.si =
   myunion.si = 0xff;
+  myunion.ei[0] = 0x2;
+  myunion.ei[1] = 0xab;
+
+  printf("16bit: %04" PRIX16 "\n", myunion.si);
+  printf(" 8bit: %02" PRIX8 "%02" PRIX8 "\n", myunion.ei[0], myunion.ei[1]);
+  
 
   return 0;
 }
